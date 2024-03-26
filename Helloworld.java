16274dd87d28f9f737bc6d403ccff078195ff767
alan <xxx@gmail.com>
2024-03-26 10:37:58 +0800

    update

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+  public static void main(String[] args) {
+    // tool 1: System.out.println()
+    System.out.println("hello world 123");
+    System.out.println("alan");
+  }
+}
